55d7c896c002693b865fa12e575ccb2ca523bf85
CharLemAznable <545541819@qq.com>
2019-09-06 00:34:08 +0800

    Refactor config key.

--- flag.go
+++ flag.go
@@ -38,11 +38,11 @@ type AppConfig struct {
 	ForceLogin  bool
 
 	AmberLoginEnabled  bool
-	AmberAppID         string
+	AmberAppId         string
 	AmberEncryptKey    string
 	AmberCookieName    string
-	AmberAmberLoginURL string
-	AmberLocalURL      string
+	AmberAmberLoginUrl string
+	AmberLocalUrl      string
 	AmberForceLogin    bool
 }
 
@@ -95,11 +95,11 @@ ConfigFile = "config.toml"
 
 	if appConfig.AmberLoginEnabled {
 		amber.ConfigInstance = amber.NewConfig(
-			amber.WithAppId(appConfig.AmberAppID),
+			amber.WithAppId(appConfig.AmberAppId),
 			amber.WithEncryptKey(appConfig.AmberEncryptKey),
 			amber.WithCookieName(appConfig.AmberCookieName),
-			amber.WithAmberLoginUrl(appConfig.AmberAmberLoginURL),
-			amber.WithLocalUrl(appConfig.AmberLocalURL),
+			amber.WithAmberLoginUrl(appConfig.AmberAmberLoginUrl),
+			amber.WithLocalUrl(appConfig.AmberLocalUrl),
 			amber.WithForceLogin(appConfig.AmberForceLogin),
 		)
 	}
